cba0a69a34650997b54119b3fb43f7f50700b650
Daniel Story <danstory@Daniels-MBP.ad.storyapps.com>
2017-09-20 17:04:15 -0600

    Fix host name call.

--- functions.psm1
+++ functions.psm1
@@ -3,7 +3,7 @@ Function Export-HostDockerMachine {
     if((Get-Command docker-machine)){
         $hostMachine = $(docker-machine ip $env:DOCKER_MACHINE_NAME)
         if($LASTEXITCODE -ne 0){
-            $hostMachine = [System.Net.Dns]::GetHostByName()
+            $hostMachine = [System.Net.Dns]::GetHostName(()
         }
     }
 
